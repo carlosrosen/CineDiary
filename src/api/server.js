a789b0daee4b240341efc10fbf434c37104ba328
Davi Roberto <davi.roberto082@gmail.com>
2026-06-04 16:32:13 -0300

    Inicializando o projeto

---
+++ src/api/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const PORT = 3000;
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.json({message: 'Hello World!'})
+})
+app.get('/a', (req,res)=>{
+  res.json({message: 'Hello World!'})
+})
+
+app.listen(PORT,()=>{
+    console.log(`Esta rodando na porta ${PORT}`)
+})
