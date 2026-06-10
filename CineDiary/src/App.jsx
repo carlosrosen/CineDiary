dae21e4e15f1f779ac25fede51c1eaf103c56695
Davi Roberto <davi.roberto082@gmail.com>
2026-06-10 02:44:54 -0300

    Criando os modais de adição/edição de registros das avaliações e de alerta para erros e ou aviso mais personalizado com a interface do site

--- CineDiary/src/App.jsx
+++ CineDiary/src/App.jsx
@@ -1,10 +1,32 @@
-import Logo from './components/Logo'
+import { useState, useEffect } from "react";
+import { AddEditMovieSerie } from "./components/addEditMovieSerie";
+import { AlertModal } from "./components/AlertModal";
 
 function App() {
+  const [showAddEditMovieSerie, setShowAddEditMovieSerie] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [messageAlert, setMessageAlert] = useState("");
+  
+  useEffect(()=>{
+    if(messageAlert === '') return;
+    const alertTimeout = async ()=>{
+      setShowAlert(true);
+      setTimeout(()=>{
+        setMessageAlert("");
+        setShowAlert(false);
+      }, 8000)
+    }
+    alertTimeout();
+    }, [messageAlert])
   return (
     <div>
+      <button onClick={() => {setShowAddEditMovieSerie(true)}}>
+        Adicionar Filme/Série
+      </button>
+      <AddEditMovieSerie setShowAddEditMovieSerie={setShowAddEditMovieSerie} showAddEditMovieSerie={showAddEditMovieSerie} setMessageAlert={setMessageAlert} />
+      <AlertModal title='Ocorreu um erro' messageAlert={messageAlert} setMessageAlert={setMessageAlert} showAlert={showAlert} setShowAlert={setShowAlert} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
